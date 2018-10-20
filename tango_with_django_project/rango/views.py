62208cceda9118cf52690f7ca15c670d453dc434
QiusenHuangGT <qiusenhuang17@hotmail.com>
2018-10-19 22:20:32 -0400

    added media service

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
 	#return HttpResponse("Hello, world. You are at the rango page!\
 	#<br/> <a href = '/rango/about/'> About </a>")
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	
+	current_time = timezone.now()
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!", \
+					'current_time': current_time}
 	return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
