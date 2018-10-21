0b17c6ef5111ac277b95cf8a1e61a6c2b475c5f8
QiusenHuangGT <qiusenhuang17@hotmail.com>
2018-10-21 18:13:18 -0400

    add database support

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from rango.models import Category
 # Create your views here.
 
 def index(request):
 	#return HttpResponse("Hello, world. You are at the rango page!\
 	#<br/> <a href = '/rango/about/'> About </a>")
-	
+	category_list = Category.objects.order_by('-likes')[:5]
 	current_time = timezone.now()
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!", \
-					'current_time': current_time}
+	context_dict = {'categories': category_list,
+					'current_time': current_time
+					}
 	return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
